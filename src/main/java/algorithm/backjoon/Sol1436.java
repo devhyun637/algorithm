643aec698c3db2141e9826d448dc67d0a88b35da
devhyun637 <kgshdgeh@gmail.com>
2020-02-18 22:10:26 +0900

    1로만들기 문제풀이

---
+++ src/main/java/algorithm/backjoon/Sol1436.java
@@ -0,0 +1,27 @@
+package algorithm.backjoon;
+
+import java.util.Scanner;
+
+//1로만들기 -Dynamic programming
+public class Sol1436 {
+    public static void main(String[] args) {
+        int[] memo = new int[10000000];
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        memo[1] = 0;
+        for (int i = 2; i <= num; i++) {
+            memo[i] = memo[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                if (memo[i / 2] + 1 < memo[i]) memo[i] = (memo[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                if (memo[i / 3] + 1 < memo[i]) memo[i] = (memo[i / 3] + 1);
+            }
+        }
+
+        System.out.println(memo[num]);
+    }
+}
