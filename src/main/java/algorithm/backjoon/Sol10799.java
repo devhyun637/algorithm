a7dc237dd9fcf3ece67c36d02286463360f20d59
devhyun637 <kgshdgeh@gmail.com>
2020-02-18 22:07:29 +0900

    쇠막대기 문제풀이

---
+++ src/main/java/algorithm/backjoon/Sol10799.java
@@ -0,0 +1,32 @@
+package algorithm.backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//쇠막대기
+public class Sol10799 {
+    public static void main(String[] args) throws Exception {
+        int answer = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') stack.push('(');
+            else {
+                if (str.charAt(i-1) == ')') {
+                    answer += 1;
+                    stack.pop();
+                } else {
+                    stack.pop();
+                    answer += stack.size();
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
