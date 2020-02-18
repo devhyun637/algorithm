821b257c5dd1b30b1d20a9b197d42361807e7547
devhyun637 <kgshdgeh@gmail.com>
2020-02-18 22:07:55 +0900

    괄호 문제풀이

---
+++ src/main/java/algorithm/backjoon/Sol9012.java
@@ -0,0 +1,42 @@
+package algorithm.backjoon;
+
+//괄호
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Sol9012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+
+        while (count-- > 0) {
+            System.out.println(psvTest(sc.next()));
+        }
+
+    }
+
+    public static String psvTest(String str) {
+        //1. 시작 문자가 ')'이라면 바로 NO를 리턴한다.
+        //2. 시작 문자가 '('이라면 stack에 push하고, ')'이라면 pop한다.
+        //   stack의 사이즈가 0이라면 NO를 리턴한다.
+        //   문자열을 다 돌았는데 stack의 사이즈가 0이라면 NO를 리턴한다.
+        String answer = "NO";
+        int count = str.length();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < count; i++) {
+            if (str.charAt(0) == ')') return answer;
+            else {
+                if (str.charAt(i) == '(') stack.push(1);
+                else {
+                    if (stack.size() == 0) return answer;
+                    else stack.pop();
+                }
+            }
+        }
+
+        if (stack.size() == 0) answer = "YES";
+        return answer;
+    }
+}
