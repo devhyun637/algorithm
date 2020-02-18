ad010cd5805f2a67614ceb3dfefbe840b7a75f3d
devhyun637 <kgshdgeh@gmail.com>
2020-02-18 22:02:38 +0900

    쇠막대기 문제풀이

---
+++ src/main/java/algorithm/programmers/Sol42585.java
@@ -0,0 +1,26 @@
+package algorithm.programmers;
+
+import java.util.Stack;
+
+public class Sol42585 {
+    public static void main(String[] args){
+        int answer = solution("()(((()())(())()))(())");
+        System.out.println(answer);
+    }
+
+    public static int solution(String arrangement) {
+        int answer = 0;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < arrangement.length(); i++) {
+            if (arrangement.charAt(i) == '(') stack.push('(');
+            else {
+                stack.pop();
+                if (arrangement.charAt(i-1) == ')') answer += 1;
+                else answer += stack.size();
+            }
+        }
+        return answer;
+    }
+}
