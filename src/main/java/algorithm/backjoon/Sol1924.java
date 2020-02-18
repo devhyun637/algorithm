51c9a1c616a8dcc729f7f307e46b3e67bb57681b
devhyun637 <kgshdgeh@gmail.com>
2020-02-18 22:10:10 +0900

    2007년 문제풀이

---
+++ src/main/java/algorithm/backjoon/Sol1924.java
@@ -0,0 +1,20 @@
+package algorithm.backjoon;
+
+import java.util.Scanner;
+
+public class Sol1924 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int firstNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+
+        int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+        for (int i = 0; i < firstNum - 1; i++) {
+            secondNum += month[i];
+        }
+
+        System.out.println(day[secondNum % 7]);
+    }
+}
