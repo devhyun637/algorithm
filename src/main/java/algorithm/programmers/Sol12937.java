e3486181c6f5eba484a016d0046bb8826a61bff4
devhyun637 <kgshdgeh@gmail.com>
2020-02-20 01:19:53 +0900

    짝수와 홀수 문제풀이

---
+++ src/main/java/algorithm/programmers/Sol12937.java
@@ -0,0 +1,13 @@
+package algorithm.programmers;
+
+//짝수와 홀수
+public class Sol12937 {
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+    }
+
+    public static String solution(int num) {
+        if (num == 0) return "Even";
+        return num%2 == 0 ? "Even" : "Odd";
+    }
+}
