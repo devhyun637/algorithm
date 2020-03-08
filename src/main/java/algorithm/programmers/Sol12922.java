c3f23a2d73cf99ae7c6e9f68a6e7e31eed89b4b3
devhyun637 <kgshdgeh@gmail.com>
2020-03-09 05:18:53 +0900

    수박수박 문제풀이

---
+++ src/main/java/algorithm/programmers/Sol12922.java
@@ -0,0 +1,17 @@
+package algorithm.programmers;
+
+public class Sol12922 {
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
+
+    public static String solution(int n) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            answer += i % 2 == 0 ? "수" : "박";
+        }
+
+        return answer;
+    }
+}
