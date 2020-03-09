68098edc654455bdfa07343698e6631283219e03
devhyun637 <kgshdgeh@gmail.com>
2020-03-09 14:02:12 +0900

    k번째 수 문제풀이

---
+++ src/main/java/algorithm/programmers/Sol42748.java
@@ -0,0 +1,23 @@
+package algorithm.programmers;
+
+import java.util.Arrays;
+
+public class Sol42748 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(Arrays.toString(solution(array, commands)));
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+}
