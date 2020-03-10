b26a5a06bfd9a8089ad45be9af50360731cfa9ed
devhyun637 <kgshdgeh@gmail.com>
2020-03-11 00:00:54 +0900

    문제 분류중

--- src/main/java/algorithm/programmers/Level1/Sol12944.java
+++ src/main/java/algorithm/programmers/Level1/Sol12944.java
@@ -1,5 +1,6 @@
-package algorithm.programmers;
+package algorithm.programmers.Level1;
 
+//평균구하기
 public class Sol12944 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
@@ -8,9 +9,7 @@ public class Sol12944 {
 
     public static double solution(int[] arr) {
         double answer = 0;
-//        for (int i = 0; i < arr.length; i++) {
-//            answer += arr[i];
-//        }
+
         for (int value : arr) {
             answer += value;
         }
