e3e50be5f99ad8955215a9ec74c2218cac6e12cd
devhyun637 <kgshdgeh@gmail.com>
2020-03-11 17:06:03 +0900

    k번째 수 문제풀이 및 문제 분류중

--- src/main/java/algorithm/programmers/Level1/Sol42748.java
+++ src/main/java/algorithm/programmers/Level1/Sol42748.java
@@ -1,18 +1,20 @@
-package algorithm.programmers;
+package algorithm.programmers.Level1;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
+//K번째 수
 public class Sol42748 {
     public static void main(String[] args) {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-        System.out.println(Arrays.toString(solution(array, commands)));
+        System.out.println(Arrays.toString(solution(array, commands))); //5,3,6
     }
 
     public static int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        for (int i = 0; i < commands.length; i++) {
+        for (int i = 0; i < answer.length; i++) {
             int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             Arrays.sort(arr);
             answer[i] = arr[commands[i][2] - 1];
