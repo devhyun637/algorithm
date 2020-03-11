e53a458c5410d6009a9f01e722b8000d20821f13
devhyun637 <kgshdgeh@gmail.com>
2020-03-11 16:52:33 +0900

    핸드폰 번호 가리기 문제풀이 및 문제 분류중

--- src/main/java/algorithm/programmers/Level1/Sol12948.java
+++ src/main/java/algorithm/programmers/Level1/Sol12948.java
@@ -1,13 +1,13 @@
-package algorithm.programmers;
+package algorithm.programmers.Level1;
 
-import java.util.Arrays;
-
+//핸드폰번호 가리기
 public class Sol12948 {
     public static void main(String[] args) {
         System.out.println(solution("01033334444"));
     }
 
     public static String solution(String phone_number) {
+        String answer = "";
         char[] ch = phone_number.toCharArray();
 
         for (int i = 0; i < ch.length - 4; i++) {
