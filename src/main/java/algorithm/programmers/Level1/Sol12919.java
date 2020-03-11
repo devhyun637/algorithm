fcafe5a4176abb97e5de21509575e633e66cf022
devhyun637 <kgshdgeh@gmail.com>
2020-03-11 16:28:54 +0900

    김서방찾기 문제풀이 및 문제 분류중

--- src/main/java/algorithm/programmers/Level1/Sol12919.java
+++ src/main/java/algorithm/programmers/Level1/Sol12919.java
@@ -1,19 +1,20 @@
-package algorithm.programmers;
+package algorithm.programmers.Level1;
 
+//김서방
 public class Sol12919 {
     public static void main(String[] args) {
         String[] seoul = {"Jane", "Kim"};
         System.out.println(solution(seoul));
     }
     public static String solution(String[] seoul) {
-        int num = 0;
+        int count = 0;
 
-        for (int i=0; i<seoul.length; i++) {
+        for (int i=0; i<seoul.length; i++){
             if (seoul[i].equals("Kim")) {
-                num = i;
+                count = i;
                 break;
             }
         }
-        return "김서방은 "+num+"에 있다";
+        return "김서방은 " + count + "에 있다";
     }
 }
