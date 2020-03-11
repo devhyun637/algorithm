9a8f5e18d9ace4bf956d0db68fa8e81362b34715
devhyun637 <kgshdgeh@gmail.com>
2020-03-11 16:56:09 +0900

    완주하지 못한 선수 문제풀이 및 문제 분류중

--- src/main/java/algorithm/programmers/Level1/Sol42576.java
+++ src/main/java/algorithm/programmers/Level1/Sol42576.java
@@ -1,7 +1,8 @@
-package algorithm.programmers;
+package algorithm.programmers.Level1;
 
 import java.util.Arrays;
 
+//완주하지 못한 선수
 public class Sol42576 {
     public static void main(String[] args) {
         String[] arr1 = {"marina", "josipa", "nikola", "vinko", "filipa"};
@@ -12,13 +13,15 @@ public class Sol42576 {
     public static String solution(String[] participant, String[] completion) {
         Arrays.sort(participant);
         Arrays.sort(completion);
+        String answer = "";
 
-        String answer = participant[participant.length - 1];
-
-        for(int i=0; i<completion.length; i++){
-            if(!participant[i].equals(completion[i])) return participant[i];
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
         }
-
+        if (answer.equals("")) return participant[participant.length - 1];
         return answer;
     }
 }
