a3fb804011b45f68e182b4fb8326b03210003f19
devhyun637 <kgshdgeh@gmail.com>
2020-03-11 16:30:58 +0900

    자릿수더하기 문제풀이 및 문제 분류중

--- src/main/java/algorithm/programmers/Level1/Sol12931.java
+++ src/main/java/algorithm/programmers/Level1/Sol12931.java
@@ -1,17 +1,19 @@
-package algorithm.programmers;
+package algorithm.programmers.Level1;
 
+//자릿수 더하기
 public class Sol12931 {
     public static void main(String[] args) {
-        System.out.println(solution(123));
+        System.out.println(solution(12345));
     }
 
     public static int solution(int n) {
         int answer = 0;
 
         while (n > 0) {
-            answer += n % 10;
-            n = n / 10;
+            answer += n%10;
+            n = n/10;
         }
+
         return answer;
     }
 }
