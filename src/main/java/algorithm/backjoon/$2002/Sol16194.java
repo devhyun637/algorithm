db8bc342fc2851a7ec5d846cde158fe8b77b37e5
devhyun637 <kgshdgeh@gmail.com>
2020-03-16 02:38:45 +0900

    카드 구매하기2 코드수정

--- src/main/java/algorithm/backjoon/$2002/Sol16194.java
+++ src/main/java/algorithm/backjoon/$2002/Sol16194.java
@@ -1,23 +1,22 @@
 package algorithm.backjoon.$2002;
 
+//카드 구매하기2
 import java.util.Scanner;
 
 public class Sol16194 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int card = sc.nextInt() + 1;
-        int[] price = new int[card - 1];
-        int[] d = new int[card];
-        d[0] = 0;
+        int[] price = new int[card];
 
-        for (int i = 0; i < card - 1; i++) {
+        for (int i = 1; i < card; i++) {
             price[i] = sc.nextInt();
-            d[i+1] = price[i];
         }
+        int[] d = price;
 
         for (int i = 1; i < card; i++) {
             for (int j = 1; j <= i; j++) {
-                d[i] = Math.min(d[i], price[j - 1] + d[i - j]);
+                d[i] = Math.min(d[i], price[j] + d[i - j]);
             }
         }
         System.out.println(d[card - 1]);
