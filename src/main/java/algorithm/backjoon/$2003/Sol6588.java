20534dacca2eb1efd007e6d6ba0d7867a85be6b9
devhyun637 <kgshdgeh@gmail.com>
2020-03-28 21:47:43 +0900

    골드바흐 추측 문제풀이(재)

--- src/main/java/algorithm/backjoon/$2003/Sol6588.java
+++ src/main/java/algorithm/backjoon/$2003/Sol6588.java
@@ -5,29 +5,31 @@ import java.util.Scanner;
 //골드바흐의 추측
 public class Sol6588 {
     public static void main(String[] args) {
-        int MAX = 10000001;
+        int MAX = 1000000;
         Scanner sc = new Scanner(System.in);
 
-        boolean[] isPrime = new boolean[MAX];
-
+        //에라토스테네스의 체
+        boolean[] isPrime = new boolean[MAX + 1];
         isPrime[0] = isPrime[1] = true;
-        for (int i = 2; i * i < MAX; i++) {
-            for (int j = i * i; j < MAX; j += i) {
-                if (isPrime[i]) break;
-                else {
-                    isPrime[j] = true;
-                }
+
+        for (int i = 2; i * i <= MAX; i++) {
+            for (int j = i * 2; j <= MAX; j += i) {
+                if (!isPrime[j]) isPrime[j] = true;
             }
         }
 
         while (true) {
             int num = sc.nextInt();
             if (num == 0) break;
-            for (int i = 1; i <= num; i++) {
+
+            for (int i = 2; i <= num; i++) {
                 if (!isPrime[i] && !isPrime[num - i]) {
-                    System.out.println(num + " = " + i + " + " + (num - i));
+                    int a = i;
+                    int b = num - i;
+                    System.out.println(num + " = " + a + " + " + b);
                     break;
                 }
+                if (i == num) System.out.println("Goldbach's conjecture is wrong.");
             }
         }
     }
