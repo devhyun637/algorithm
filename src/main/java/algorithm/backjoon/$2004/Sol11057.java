49101421a93adf24f412966f1b62a45526db2f34
devhyun637 <kgshdgeh@gmail.com>
2020-04-04 05:01:42 +0900

    오르막수 문제풀이 수정

--- src/main/java/algorithm/backjoon/$2004/Sol11057.java
+++ src/main/java/algorithm/backjoon/$2004/Sol11057.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 //오르막 수
 public class Sol11057 {
     public static void main(String[] args) {
-        final int MAX = 1000;
-        long[][] d = new long[MAX + 1][10];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long answer = 0;
+        long[][] d = new long[n + 1][10];
 
         for (int i = 1; i < d.length; i++) {
             for (int j = 0; j < d[i].length; j++) {
@@ -17,18 +19,14 @@ public class Sol11057 {
                             d[i][j] = 1;
                             break;
                         }
-                        d[i][j] += (d[i - 1][k] % 10007);
+                        d[i][j] += d[i - 1][k] % 10007;
                     }
                 }
             }
         }
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long answer = 0;
-
         for (long l : d[n]) {
-            answer += (l % 10007);
+            answer += l % 10007;
         }
 
         System.out.println(answer % 10007);
