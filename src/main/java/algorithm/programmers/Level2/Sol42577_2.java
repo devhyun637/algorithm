79a1d682a50890e8d8efb139159dad6ab2f81c36
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-07-02 11:38:45 +0900

    전화번호 문제풀이2

---
+++ src/main/java/algorithm/programmers/Level2/Sol42577_2.java
@@ -0,0 +1,24 @@
+package algorithm.programmers.Level2;
+
+import java.util.Arrays;
+
+//전화번호 목록2
+public class Sol42577_2 {
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        System.out.println(solution(phone_book));
+    }
+
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = i+1; j < phone_book.length; j++) {
+                if (phone_book[i].startsWith(phone_book[j]) || phone_book[j].startsWith(phone_book[i])) return false;
+            }
+        }
+
+        return answer;
+    }
+}
