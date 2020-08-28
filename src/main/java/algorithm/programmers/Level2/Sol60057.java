48907ed2603363f6f8aff15749c6e24f343aa055
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-08-29 03:10:49 +0900

    문자열압축 문제풀이

--- src/main/java/algorithm/programmers/Level2/Sol60057.java
+++ src/main/java/algorithm/programmers/Level2/Sol60057.java
@@ -13,47 +13,47 @@ public class Sol60057 {
         //길이가 1인경우 바로 리턴한다.
         if (s.length() == 1) return 1;
 
-        int answer = s.length(), arrayLen, count = 0;
+        int answer, arrayLen, count = 0;
         int len = s.length();
         int[] findMin = new int[len / 2];
 
         for (int i = 0; i < len / 2; i++) {
-            //문자열의 길
+            //문자의 길이
             count++;
 
-            //글자수의 길이만큼 배열의 길이를 설정해준다.
+            //글자수의 길이만큼 배열의 크기를 설정해준다.
             if (len % count != 0) arrayLen = len / count + 1;
             else arrayLen = len / count;
-            String[] str = new String[arrayLen];
+            String[] strArray = new String[arrayLen];
 
 
             //문자열을 자른다.
             int index = 0;
-            for (int j = 0; j < str.length; j++) {
-                //StringIndexOutOfBoundsException 방
-                if (index + count > len) str[j] = s.substring(index, len);
-                else str[j] = s.substring(index, index + count);
+            for (int j = 0; j < strArray.length; j++) {
+                //StringIndexOutOfBoundsException 오류 방지
+                if (index + count > len) strArray[j] = s.substring(index, len);
+                else strArray[j] = s.substring(index, index + count);
                 index += count;
             }
 
-//            System.out.println(Arrays.toString(str));
+//            System.out.println(Arrays.toString(strArray));
 
             int counting = 1;
             String newStr = "";
-            for (int j = 0; j < str.length - 1; j++) {
-                if (str[j].equals(str[j + 1])) {
+            for (int j = 0; j < strArray.length - 1; j++) {
+                if (strArray[j].equals(strArray[j + 1])) {
                     counting++;
                 } else {
-                    if (counting != 1) newStr += counting + str[j];
-                    else newStr += str[j];
+                    if (counting != 1) newStr += counting + strArray[j];
+                    else newStr += strArray[j];
                     counting = 1;
                 }
             }
-            if (counting != 1) newStr += counting + str[str.length - 1];
-            else newStr += str[str.length - 1];
+
+            if (counting != 1) newStr += counting + strArray[strArray.length - 1];
+            else newStr += strArray[strArray.length - 1];
 
             findMin[i] = newStr.length();
-//            answer = Math.min(newStr.length(), answer);
         }
 
         Arrays.sort(findMin);
